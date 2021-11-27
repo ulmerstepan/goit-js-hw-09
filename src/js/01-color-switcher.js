8900b77a69a2073dc68e3ef6a11c225f0ea6c3a0
Stepan Ulmer <ulmer.st@gmail.com>
2021-11-27 18:28:02 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+let colorChangeInterval = null;
+
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+    colorChangeInterval = setInterval(changeBackground, 1000);
+    btnStart.disabled = true;
+};
+
+function onBtnStopClick() {
+    clearInterval(colorChangeInterval);
+    btnStart.disabled = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackground() {
+   document.body.style.background = getRandomHexColor();
+}
 
